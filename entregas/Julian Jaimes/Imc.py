70f849bf468e7b44bc947e61b4bae783a85babc0
Jotajaimes <102336715+Jotajaimes@users.noreply.github.com>
2023-11-11 11:57:21 -0500

    Create Imc.py

---
+++ entregas/Julian Jaimes/Imc.py
@@ -0,0 +1,8 @@
+print  ("Ingrese su peso")
+Peso= float(input())
+
+print  ("Ingrese su altura")
+Altura= float(input())
+
+Imc=Peso/(Altura*Altura)
+print('Tu indice de masa corporal es {}'.format(Imc))
