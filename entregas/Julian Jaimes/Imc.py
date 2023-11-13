3d23cb7c77a65f6f5306291d0307056499b51bba
Jotajaimes <102336715+Jotajaimes@users.noreply.github.com>
2023-11-13 15:42:25 -0500

    Delete entregas/Julian Jaimes directory

--- entregas/Julian Jaimes/Imc.py
+++
@@ -1,8 +0,0 @@
-print  ("Ingrese su peso")
-Peso= float(input())
-
-print  ("Ingrese su altura")
-Altura= float(input())
-
-Imc=Peso/(Altura*Altura)
-print('Tu indice de masa corporal es {}'.format(Imc))
