f30f85b47f92f51dab1904e932bccc7f2e136d45
Yursksf1 <yurs.ksf1@gmail.com>
2023-11-14 21:01:34 -0500

    agregar admin nombre, edad

--- proyectos-django/myapp/admin.py
+++ proyectos-django/myapp/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from myapp.models import Usuarios
 # Register your models here.
 
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'edad']
 
-admin.site.register(Usuarios)
+admin.site.register(Usuarios, UsuarioAdmin)
