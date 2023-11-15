0903b4726bfefeea8099dea8688aee4bd56a9c46
Yursksf1 <yurs.ksf1@gmail.com>
2023-11-14 20:59:14 -0500

    agregar nombre model

--- proyectos-django/myapp/models.py
+++ proyectos-django/myapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Usuarios(models.Model):
     nombre = models.CharField(max_length=200)
     edad = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.nombre
