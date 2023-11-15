70c2d58cf298a9eb191a8de90469764fb60e8574
Yursksf1 <yurs.ksf1@gmail.com>
2023-11-14 19:19:29 -0500

    ejemplo lista de diccionarios

---
+++ ejericicio_3_listas.py
@@ -0,0 +1,25 @@
+my_lista = []
+
+for i in range(5):
+    nombre = input('ingrese nombre {}: '.format(i))    
+    edad = float(input('ingrese edad {}: '.format(i)))
+    my_lista.append(
+        {
+          "nombre": nombre,  
+          "edad": edad,  
+        }
+    )
+
+print('imprimendo la lista completa')
+for i in my_lista:
+    print(i)
+
+
+print('calcular promedio de edad')
+suma_edad = 0.0
+for i in my_lista:
+    suma_edad = suma_edad + i['edad']
+
+print('total de edades: ', suma_edad)
+print('total de registros: ', len(my_lista))
+print('promedios de edades: ', suma_edad/len(my_lista))
