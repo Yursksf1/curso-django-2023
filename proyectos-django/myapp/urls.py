3feb7fdf544943d6d885e54dfc772f29e936b444
Yursksf1 <yurs.ksf1@gmail.com>
2023-11-16 20:07:45 -0500

    definir mi primera vista holamundo django

--- proyectos-django/myapp/urls.py
+++ proyectos-django/myapp/urls.py
@@ -4,4 +4,5 @@ from myapp import views
 
 urlpatterns = [
     path('', views.index ),
+    path('home', views.home ),
 ]
--- proyectos-django/myapp/views.py
+++ proyectos-django/myapp/views.py
@@ -8,3 +8,6 @@ from django.http import (
 
 def index(request):
     return HttpResponse('Hola mundo')
+
+def home(request):
+    return HttpResponse('<h1> Bienvenido al curso de django 2023 </h1>')
