44990cce80e8a6072ffd879dc9c9dafb38767377
Yursksf1 <yurs.ksf1@gmail.com>
2023-11-16 19:39:20 -0500

    definir mi primera vista holamundo django

--- proyectos-django/myapp/models.py
+++ proyectos-django/myapp/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Usuarios(models.Model):
+class Usuario(models.Model):
     nombre = models.CharField(max_length=200)
     edad = models.PositiveIntegerField()
     altura = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
 
     def __str__(self) -> str:
         return self.nombre
+    
+
+class HorasTrabajada(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    total_horas = models.PositiveIntegerField()
+    dia = models.DateField()
+
+    def __str__(self) -> str:
+        return "{} {} {}".format(self.usuario.nombre, self.total_horas, self.dia)
+
