49f70a475d5244a10c54878d4c18ae0fc5ab855f
Jcgonzalezb721 <107662814+Jcgonzalezb721@users.noreply.github.com>
2023-11-21 07:59:38 -0500

    Delete entregas/Julio Gonzalez/myapp directory

--- entregas/Julio Gonzalez/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from myapp.models import Usuarios, HorasTrabajadas
-# Register your models here.
-
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ['id','nombre','apellido','edad','altura','peso']
-    
-admin.site.register(Usuarios, UsuarioAdmin)
-admin.site.register(HorasTrabajadas)
