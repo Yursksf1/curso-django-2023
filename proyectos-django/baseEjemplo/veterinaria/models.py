a028c458454c44c5174594886e0dcc2605ea8ec5
Yursksf1 <yurs.ksf1@gmail.com>
2023-11-23 20:59:23 -0500

    add ejercicio

--- proyectos-django/baseEjemplo/veterinaria/models.py
+++ proyectos-django/baseEjemplo/veterinaria/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
-# Create your models here.
 class Propietario(models.Model):
     nombre = models.CharField(max_length=200)
+    celular_cli = models.PositiveIntegerField(null=True)
+    email_cli = models.EmailField(max_length=200)
+    fecha_nacimiento_cli = models.DateField(default=None, null=True)
 
     def __str__(self) -> str:
         return self.nombre
-    
 
-# Create your models here.
 class Mascota(models.Model):
     propietario = models.ForeignKey(Propietario, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=200)
+    fecha_nacimiento_mas = models.DateField(default=None, null=True)
+    especie_mas = models.CharField(max_length=200)
+    peso_mas = models.DecimalField(max_digits=5, decimal_places=2, default=20)
 
     def __str__(self) -> str:
         return self.nombre
--- proyectos-django/baseEjemplo/veterinaria/views.py
+++ proyectos-django/baseEjemplo/veterinaria/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import (
+    Http404,
+    HttpResponse,
+    HttpResponsePermanentRedirect,
+    HttpResponseRedirect,
+)
 # Create your views here.
+
+def index(request):
+    messsage = "Hola mundo aca vamos a renderizar los registros de los propietarios"
+
+
+    # Your code is here
+    '''
+    nombre de los propietarios en un texto
+    '''
+
+    return HttpResponse(messsage)
