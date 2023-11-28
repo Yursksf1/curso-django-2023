deb178358c38a636df89756bbc435b98bc658e5c
Jottajaimes <julianjaimesotero@gmail.com>
2023-11-27 21:08:11 -0500

    Bubir actualización

--- entregas/Julian Jaimes/Semana 3/myapp/views.py
+++ entregas/Julian Jaimes/Semana 3/myapp/views.py
@@ -20,14 +20,39 @@ def index(request):
     return HttpResponse(mensaje)
  
 def Horas(request):
-    mensaje_respuesta = '<h1> Calculo pago usurio </h1> </br>'
+    mensaje_respuesta = '<h1> Calculo de pago  </h1> </br>'
     usuarios = suscriptore.objects.all()
     for usuario in usuarios:
+        Vlor_sin=0
         total_horas_trabajadas=0
+        print(usuario)
         horas_trabajadas=usuario.horas_entrenamiento_set.all()
+        print( horas_trabajadas)
         for ht in horas_trabajadas:
-            total_horas_trabajadas=total_horas_trabajadas + ht.total_horas 
-            Valor_horas=total_horas_trabajadas 
-        mensaje_respuesta = "{} Mi nombre es {}, He trabajado {} horas extras y el valor es de $ {}</br>".format(mensaje_respuesta, usuario.nombre,total_horas_trabajadas,Valor_horas)           
+            print(ht)
+            horas_trabajadas=total_horas_trabajadas + ht.total_horas 
+            t_hor_trabaja=horas_trabajadas
+           # print(t_hor_trabajas)
+        #mensaje_respuesta = "{} Mi nombre es {}, llevo entrenado {} horas, el valor es mi factura es {}</br>".format(mensaje_respuesta, usuario.nombre,total_horas_trabajadas,Valor_horas)           
+        Vlor_sin=5000*t_hor_trabaja
+        if t_hor_trabaja<=15:
+            print("menor 15")
+            Vlor_des=5000*t_hor_trabaja
+            Des=Vlor_sin-Vlor_des
+            Tipo='Bronce'
+        elif t_hor_trabaja>=15 and t_hor_trabaja<=30:
+            print("entre 15 y 30") 
+            Vlor_des=3500*t_hor_trabaja
+            Des=Vlor_sin-Vlor_des
+            Tipo='Plata'
+        else:
+            print("Mayor 30")
+            Vlor_des=2000*t_hor_trabaja
+            Des=Vlor_sin-Vlor_des
+            Tipo='Oro'
+        mensaje_respuesta = "{} Tu nombre es {}, llevas entrenado {} horas, el valor de tu factura es {} y el descuento por cliente {} es de {}</br>".format(mensaje_respuesta, usuario.nombre,t_hor_trabaja,Vlor_des,Tipo,Des)
+    
 
+        
+                
     return HttpResponse(mensaje_respuesta)
