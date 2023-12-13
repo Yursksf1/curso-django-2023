339ffca3be250d7167ce51e79b11cce059306205
Yursksf1 <yurs.ksf1@gmail.com>
2023-12-12 19:50:59 -0500

    cambio de rutas dinamicas

--- proyectos-django/Navidad/myapp/urls.py
+++ proyectos-django/Navidad/myapp/urls.py
@@ -3,15 +3,11 @@ from django.urls import path, include
 from myapp import views
 
 
-
-
+app_name = "app"
 urlpatterns = [
-    path('', views.index),
-    path('My_Navidad', views.My_Navidad ),
-    path('Canciones', views.Canciones ),
-    path('Cocina', views.Cocina ),
-    path('Regalos', views.Regalos),
-    
-
-    
+    path('', views.index, name='index'),
+    path('My_Navidad', views.My_Navidad, name='my_navidad'),
+    path('Cancion', views.Canciones, name='canciones' ),
+    path('Cocina', views.Cocina, name='cocina' ),
+    path('Regalos', views.Regalos, name='regalos'),    
 ]
